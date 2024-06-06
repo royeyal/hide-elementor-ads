53fae8fb684db51e61f834e2958194e3d9c854f6
Roy Eyal <roy.eyal@gmail.com>
2024-06-06 12:24:13 +0300

    Update hide-elementor-ads.php

--- hide-elementor-ads.php
+++ hide-elementor-ads.php
@@ -1,18 +1,5 @@
 <?php
-
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              https://royeyal.studio
- * @since             1.0.0
- * @package           Hide_Elementor_Ads
- *
- * @wordpress-plugin
  * Plugin Name:       Hide Elementor Ads
  * Plugin URI:        https://github.com/royeyal/hide-elementor-ads
  * Description:       Hide Elementor’s Image optimization & AI ads. CSS code by foxscribbler.com
@@ -30,7 +17,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 function hide_elementor_ads_for_elementor_editor() {
-    wp_register_style('hea_elementor_editor_css', plugins_url('/css/hide-nag-backend.css', __FILE__), false, '1.0.2');
+    wp_register_style('hea_elementor_editor_css', plugins_url('/css/hide-nag-backend.css', __FILE__), false, '1.0.3');
     wp_enqueue_style('hea_elementor_editor_css');
 }
 add_action('elementor/editor/after_enqueue_styles', 'hide_elementor_ads_for_elementor_editor');
